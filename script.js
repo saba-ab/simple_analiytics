b4d2bb0a6406738ec75fb6586a6dc31f826015ab
saba-ab <sabaabjandadze@gmail.com>
2023-04-14 06:28:54 +0400

    added dashboard and settings pages

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("dashboard").addEventListener("click", function () {
+  var url = "dashboard.html";
+  window.location.href = url;
+});
+
+document.getElementById("logout").addEventListener("click", function () {
+  var url = "index.html";
+  window.location.href = url;
+});
+
+document.getElementById("settings").addEventListener("click", function () {
+  var url = "settings.html";
+  window.location.href = url;
+});
